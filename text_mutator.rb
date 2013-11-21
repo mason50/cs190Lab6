c42813e8313be8d72b8c5a4224cedc7b5a254b06
Hanqi Du <du60@purdue.edu>
2013-11-21 13:56:34 -0500

    add swapcase option

--- text_mutator.rb
+++ text_mutator.rb
@@ -1,7 +1,7 @@
 def main
   # check the number of commandline arguments
   if(ARGV.length < 1)
-    puts "Usage: ruby text_mutator.rb [--reverse --upper --lower] <input text>"
+    puts "Usage: ruby text_mutator.rb [--reverse --upper --lower --swapcase] <input text>"
     exit
   end
   
@@ -17,8 +17,8 @@ def main
     mod_text = reverse_mod text
   when "--upper"
     mod_text = upper_mod text
-  when "--lower"
-    mod_text = lower_mod text
+  when "--swapcase"
+    mod_text = swapcase_mod text
   else
     puts "ERROR: Unrecognized modifier!"
     exit
@@ -43,4 +43,8 @@ def lower_mod input
 end
 
 # Call main method
+def swapcase_mod input
+  input.swapcase
+end
 main
+
